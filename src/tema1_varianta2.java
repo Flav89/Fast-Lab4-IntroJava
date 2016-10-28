ecc412ef84929393b353f9c30704b580d3b654cf
fav <fav.petr@gmail.com>
2016-10-28 16:12:38 +0300

    teme lab 4 ( 6/11)

--- src/tema1_varianta2.java
+++ src/tema1_varianta2.java
@@ -3,13 +3,13 @@ public class tema1_varianta2 {
 
     public static void main(String[] args) {
         int[] a = {1,3,4,6,7,8,10,12,14,23};
-        int key = SkeletonJava.readIntGUI("Choose a number you would like to search:");
+        int n = SkeletonJava.readIntGUI("Choose a number you would like to search:");
 
         int i = 0;
         boolean found = false;
 
         for (i = 0; i < a.length; i++) {
-            if (a[i] == key) {
+            if (a[i] == n) {
                 found = true;
                 break;
             }
@@ -17,7 +17,7 @@ public class tema1_varianta2 {
 
         if (found)
         {
-            SkeletonJava.printGUI("Found " + key + " at index " + i + ".");
+            SkeletonJava.printGUI("Found " + n + " at index " + i + ".");
         } else {
             SkeletonJava.printGUI("-1");
         }
